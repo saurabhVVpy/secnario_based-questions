8df4e78c3eeb59fcaf36161ab325151f004c35c7
prathamesh <prathsaw50@gmail.com>
2022-11-09 11:55:07 +0530

    new code added for fibonacci series

--- fibo_series.py
+++ fibo_series.py
@@ -1,7 +1,6 @@
-
 def fibo_series():
     try:
-        length=int(input('Tell the length of series: '))
+        length = int(input('Tell the length of series: '))
     except:
         print("*************Enter Valid Number****************** ")
         fibo_series()
@@ -9,13 +8,26 @@ def fibo_series():
     initials = [0, 1]
     result = '0 1'
 
-    for i in range(length-2):
-
-        result = result+' '+str(sum(initials))
-        initials = [initials[1],sum(initials)]
+    for i in range(length - 2):
+        result = result + ' ' + str(sum(initials))
+        initials = [initials[1], sum(initials)]
     return result
 
-print( fibo_series() )
+
+print(fibo_series())
 
 
+# code changes by prathamesh
+def fib_number():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
+
+ans = fib_number()
+
+for i in ans:
+    if i > 100:
+        break
+    print(i)
