78e357dc4179e1d70b90deb65b6ee42ec84bdc58
AniGavade <108184546+AniGavade@users.noreply.github.com>
2022-11-14 13:30:09 +0530

    Factorial series (#6)
    * fib commit by ani
    
    * New factorial programs
    
    Co-authored-by: saurabhVVpy <108184563+saurabhVVpy@users.noreply.github.com>

---
+++ factorial.py
@@ -0,0 +1,16 @@
+#  Factorial approaches
+#  by using reduce with lambda function.
+
+from functools import reduce
+
+# x = 5
+# v = reduce(lambda x, y: x*y, [1, 1] if x == 0 else range(1, x+1))
+# print(v)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# res = reduce(lambda x, y: x*y, range(1, 6))
+# print(res)
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
