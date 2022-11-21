5d5365ee23b6a6f3f86f08aab0d057f2ffe3ab50
saurabhVVpy <108184563+saurabhVVpy@users.noreply.github.com>
2022-11-21 13:54:25 +0530

    Add files via upload

---
+++ prime_num.py
@@ -0,0 +1,23 @@
+def prime():
+    try:
+        number_=int(input('Enter num: '))
+    except:
+        return prime()
+    
+    prime_num=[]    
+    
+    for num in range(number_+1):
+       
+        if number_<=1:
+            return 'number should be greater than 1 '
+            
+        elif number_==2:
+            return 2
+            
+        elif all([ num % i != 0 for i in range(2,int(num**0.5)+1) ]):
+            if num>=2:
+                prime_num.append(num)
+            
+    return prime_num    
+
+print(prime())
